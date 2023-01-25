7d112f787956f02d6e358f2ce365739130687e21
AdamWasilewskiTC <awasilewski3@tm1.edu.pl>
2023-01-25 13:09:21 +0100

    bydgoszcz

---
+++ app/src/main/java/com/example/bankapp/LoginActivity.kt
@@ -0,0 +1,70 @@
+package com.example.bankapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.bankapp.databinding.ActivityLoginBinding
+import org.json.JSONArray
+import org.json.JSONObject
+
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.login.setOnClickListener {
+            sendUserDataApi()
+        }
+    }
+        private fun sendUserDataApi() {
+            val userName = binding.pesel.text;
+            val userPass = binding.password.text;
+            val requestBodyObject = JSONObject()
+            requestBodyObject.put("pesel", userName)
+            requestBodyObject.put("passwd", userPass)
+            val requestBody = requestBodyObject.toString()
+
+            val url = "http://192.168.1.108:8080/api/sessions"
+
+            val request = object : StringRequest(
+                Method.POST,
+                url,
+                { response: String ->
+                    if(response == "")
+                        throw RuntimeException("BladLogowania.exe")
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("Token",response);
+
+                    startActivity(intent)
+                },
+                { error: VolleyError ->
+                    error.printStackTrace();
+                }
+            ){
+                override fun getBody(): ByteArray{
+                    return requestBody.toByteArray()
+                }
+
+                override fun getBodyContentType(): String {
+                    return "application/json"
+
+                }
+            }
+            val queue = Volley.newRequestQueue(this)
+            queue.add(request)
+        }
+
+
+
+}
---
+++ app/src/main/java/com/example/bankapp/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.bankapp
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.bankapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var token: String
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        token = intent.getStringExtra("Token").toString();
+        binding.Blik.setOnClickListener{
+            showBlikCode()
+        }
+    }
+
+
+    private fun showBlikCode() {
+        binding.Blik.isEnabled = false
+        binding.Blik.isClickable = false
+        getBlikCodeFromApi()
+        startTimer()
+    }
+
+    private fun getBlikCodeFromApi(): String {
+        val url = "http://192.168.1.108:8080/api/blik?sessionToken=" + token;
+        var blik = ""
+
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            { response ->
+                Log.e("myMessage", response)
+                setBlikCodeToElement(response.toString())
+            },
+            {
+                    response -> Log.e("myMessage", response.toString())
+                blik = response.toString()
+                Log.e("myMessage", blik)
+            }
+        )
+
+        val queue = Volley.newRequestQueue(this)
+        queue.add(request)
+        return blik
+    }
+
+    private fun setBlikCodeToElement(blikCode: String) {
+        Log.e("showBlikCode", "Show")
+        binding.BlikCode.visibility = View.VISIBLE
+        binding.BlikCode.text = blikCode
+    }
+
+    private fun startTimer(){
+        binding.Time.visibility = View.VISIBLE
+        binding.Time.text = "1:59"
+
+        val timerTextView = binding.Time
+
+        val timer = object : CountDownTimer(120000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val minutes = millisUntilFinished / 60000
+                val seconds = (millisUntilFinished % 60000) / 1000
+
+                timerTextView.text = "$minutes:$seconds"
+            }
+            override fun onFinish() {
+                timerTextView.text = "Time's up!"
+                binding.BlikCode.visibility = View.GONE
+                binding.BlikCode.text = ""
+                binding.Blik.isEnabled = true
+                binding.Blik.isClickable = true
+            }
+    }
+        timer.start()
+    }
+}
